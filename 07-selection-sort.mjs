4a74d0cfd24047d574224d08067ac8e44ee7216f
fcintra3 <github3@faustocintra.com.br>
2021-03-23 01:09:56 +0000

    (22/03) Início do quicksort

--- 07-selection-sort.mjs
+++ 07-selection-sort.mjs
@@ -37,9 +37,9 @@ function selectionSort(vetor) {
     }
 }
 
-//let nums = [7, 4, 9, 0, 6, 1, 8, 2, 5, 3]
+let nums = [7, 4, 9, 0, 6, 1, 8, 2, 5, 3]
 //let nums = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
-let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+//let nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 selectionSort(nums)
 console.log(nums)
 console.log({trocas, pass, comps})
